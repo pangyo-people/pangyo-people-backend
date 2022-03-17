170cf18da3c883d1c7e9ff0329129609bfcb4b6e
Ayoon-b <wnr0303@gmail.com>
2022-03-17 15:47:02 +0900

    feat: EventController 추가

---
+++ src/main/java/com/pangyopeoplebackend/event/controller/EventController.java
@@ -0,0 +1,25 @@
+package com.pangyopeoplebackend.event.controller;
+
+import com.pangyopeoplebackend.event.Event;
+import com.pangyopeoplebackend.event.service.EventService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
+@RequiredArgsConstructor
+public class EventController {
+
+    private final EventService eventService;
+
+    @GetMapping(path = "/v1/api/events")
+    public List<Event> getProduct(){
+        return eventService.getEvents();
+    }
+
+}
