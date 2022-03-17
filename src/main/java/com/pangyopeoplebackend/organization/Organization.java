e9a694652c2d60573ea89b165eecc6f59ccf5bfc
Ayoon-b <wnr0303@gmail.com>
2022-03-17 15:47:23 +0900

    feat: Organization 테이블 추가

--- src/main/java/com/pangyopeoplebackend/organization/Organization.java
+++ src/main/java/com/pangyopeoplebackend/organization/Organization.java
@@ -1,4 +1,50 @@
 package com.pangyopeoplebackend.organization;
 
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import java.util.UUID;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Getter
 public class Organization {
+    @Id
+    private String org_id;
+
+    @Column(nullable = false)
+    private String org_name;
+
+    @Column
+    private String org_category;
+
+    //@Column
+    //@Convert(converter = Category.CategoryConverter.class // enum으로 수정 필요
+    //private Category category;
+
+    @Column
+    private String org_description;
+
+    @Column
+    private String org_url;
+
+    @Column
+    private boolean org_permission;
+
+    @Column
+    private String org_created; // 만든 날짜로 수정
+
+
+    @PrePersist
+    public void prePersist() {
+        if (org_id == null) {
+            org_id = UUID.randomUUID().toString();
+        }
+    }
 }
