c59006bbcc5d09dd5112a4f65c0f15db81b82f82
Ayoon-b <wnr0303@gmail.com>
2022-03-17 15:46:43 +0900

    feat: EventService, getEvents 추가

---
+++ src/main/java/com/pangyopeoplebackend/event/service/EventService.java
@@ -0,0 +1,20 @@
+package com.pangyopeoplebackend.event.service;
+
+import com.pangyopeoplebackend.event.Event;
+import com.pangyopeoplebackend.event.repository.EventRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class EventService {
+    private final EventRepository eventRepository;
+
+    public List<Event> getEvents() {
+        return eventRepository.findAll();
+    }
+}
