1e18f978df282e59766e14adfc2ba0907eeab154
Ayoon-b <wnr0303@gmail.com>
2022-03-21 20:11:24 +0900

    feat: org_id, org_category, org_created 수정

--- src/main/java/com/pangyopeoplebackend/organization/Organization.java
+++ src/main/java/com/pangyopeoplebackend/organization/Organization.java
@@ -1,11 +1,12 @@
 package com.pangyopeoplebackend.organization;
 
+import com.pangyopeoplebackend.domain.OrgCategory;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.PrePersist;
+import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
@@ -14,19 +15,17 @@ import java.util.UUID;
 @AllArgsConstructor
 @ToString
 @Getter
+@EntityListeners(AuditingEntityListener.class)
 public class Organization {
     @Id
-    private String org_id;
+    private String org_id = UUID.randomUUID().toString();
 
     @Column(nullable = false)
     private String org_name;
 
     @Column
-    private String org_category;
-
-    //@Column
-    //@Convert(converter = Category.CategoryConverter.class // enum으로 수정 필요
-    //private Category category;
+    @Convert(converter = OrgCategory.CategoryConverter.class)
+    private OrgCategory org_category;
 
     @Column
     private String org_description;
@@ -38,7 +37,8 @@ public class Organization {
     private boolean org_permission;
 
     @Column
-    private String org_created; // 만든 날짜로 수정
+    @CreatedDate
+    private LocalDateTime org_created;
 
 
     @PrePersist
