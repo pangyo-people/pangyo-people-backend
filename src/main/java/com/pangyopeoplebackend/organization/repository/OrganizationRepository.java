3716d380089b07530871575657988f87d433c49d
Ayoon-b <wnr0303@gmail.com>
2022-03-21 23:12:53 +0900

    feat: findByOrgCategory 추가

--- src/main/java/com/pangyopeoplebackend/organization/repository/OrganizationRepository.java
+++ src/main/java/com/pangyopeoplebackend/organization/repository/OrganizationRepository.java
@@ -1,9 +1,14 @@
 package com.pangyopeoplebackend.organization.repository;
 
+import com.pangyopeoplebackend.domain.OrgCategory;
 import com.pangyopeoplebackend.organization.Organization;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 
 public interface OrganizationRepository extends JpaRepository<Organization, String>{
+
+    List<Organization> findByOrgCategory(OrgCategory orgCategory);
 }
