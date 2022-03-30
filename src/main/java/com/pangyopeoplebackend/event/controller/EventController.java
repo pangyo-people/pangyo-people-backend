17fb7c7784c7fc398de4a85182c62a71f3b07858
Ayoon-b <wnr0303@gmail.com>
2022-03-30 23:13:02 +0900

    feat: 카테고리 별 이벤트 목록 조회, url 매핑값은 추후 수정 예정

--- src/main/java/com/pangyopeoplebackend/event/controller/EventController.java
+++ src/main/java/com/pangyopeoplebackend/event/controller/EventController.java
@@ -1,7 +1,10 @@
 package com.pangyopeoplebackend.event.controller;
 
+import com.pangyopeoplebackend.domain.EventCategory;
+import com.pangyopeoplebackend.domain.OrgCategory;
 import com.pangyopeoplebackend.event.Event;
 import com.pangyopeoplebackend.event.service.EventService;
+import com.pangyopeoplebackend.organization.Organization;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 
 @RestController
@@ -22,6 +26,11 @@ public class EventController {
     @GetMapping(path = "/v1/api/events")
     public List<Event> getEvents(){
         return eventService.getEvents();
+    }
+
+    @GetMapping("/v1/api/events/category/{number}")
+    public List<Event> getEventsCategory(@PathVariable("number") EventCategory eventCategory) {
+        return eventService.getEventsByCategory(eventCategory);
     }
 
     @GetMapping()
--- src/main/java/com/pangyopeoplebackend/event/repository/EventRepository.java
+++ src/main/java/com/pangyopeoplebackend/event/repository/EventRepository.java
@@ -1,9 +1,12 @@
 package com.pangyopeoplebackend.event.repository;
 
+import com.pangyopeoplebackend.domain.EventCategory;
 import com.pangyopeoplebackend.event.Event;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface EventRepository extends JpaRepository<Event, String>{
-
+    List<Event> findByEventCategoryOrderByEventDateDesc(EventCategory eventCategory);
 }
