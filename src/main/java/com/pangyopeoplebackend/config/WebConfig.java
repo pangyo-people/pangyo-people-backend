4b9ccf0f2eb721f7a85988cd5a295343cc658383
Ayoon-b <wnr0303@gmail.com>
2022-03-31 23:22:25 +0900

    fix: WebMvcConfigurer 수정

--- src/main/java/com/pangyopeoplebackend/config/WebConfig.java
+++ src/main/java/com/pangyopeoplebackend/config/WebConfig.java
@@ -18,7 +18,11 @@ public class WebConfig implements WebMvcConfigurer {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("*");
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
         };
     }
