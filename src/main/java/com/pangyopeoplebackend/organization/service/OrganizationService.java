39df98fc286431f5519f4ab61169a5168698d329
Ayoon-b <wnr0303@gmail.com>
2022-04-19 20:35:30 +0900

    fix: 조직 전체 조회 정렬 추가

--- src/main/java/com/pangyopeoplebackend/organization/service/OrganizationService.java
+++ src/main/java/com/pangyopeoplebackend/organization/service/OrganizationService.java
@@ -4,6 +4,7 @@ import com.pangyopeoplebackend.domain.OrgCategory;
 import com.pangyopeoplebackend.organization.Organization;
 import com.pangyopeoplebackend.organization.repository.OrganizationRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -16,7 +17,7 @@ public class OrganizationService {
     private final OrganizationRepository organizationRepository;
 
     public List<Organization> getOrganizations() {
-        return organizationRepository.findAll();
+        return organizationRepository.findAll(Sort.by(Sort.Direction.ASC, "orgName"));
     }
 
     public List<Organization> getOrganizationsByOrgCategory(OrgCategory orgCategory) {
