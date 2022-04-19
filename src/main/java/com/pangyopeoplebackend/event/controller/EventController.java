dfb7c48d2184e7b48b936edc59765326f3ec07c7
Ayoon-b <wnr0303@gmail.com>
2022-04-19 17:34:43 +0900

    fix: 컨트롤러 오류 수정

--- src/main/java/com/pangyopeoplebackend/event/controller/EventController.java
+++ src/main/java/com/pangyopeoplebackend/event/controller/EventController.java
@@ -1,18 +1,11 @@
 package com.pangyopeoplebackend.event.controller;
 
-import com.pangyopeoplebackend.domain.EventCategory;
-import com.pangyopeoplebackend.domain.OrgCategory;
 import com.pangyopeoplebackend.event.Event;
 import com.pangyopeoplebackend.event.service.EventService;
-import com.pangyopeoplebackend.organization.Organization;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.util.List;
-import java.util.Optional;
 
 
 @RestController
@@ -24,11 +17,6 @@ public class EventController {
     @GetMapping(path = "/v1/api/events")
     public List<Event> getEvents(){
         return eventService.getEvents();
-    }
-
-    @GetMapping("/v1/api/events/category/{number}")
-    public List<Event> getEventsByCategory(@PathVariable int number) {
-        return eventService.getEventsByCategory(number);
     }
 
     @PostMapping("/v1/api/events/write")
