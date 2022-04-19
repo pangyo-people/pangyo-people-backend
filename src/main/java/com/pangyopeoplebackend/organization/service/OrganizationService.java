2038519c93d3567861b8afec63123e8536fbd59b
Ayoon-b <wnr0303@gmail.com>
2022-04-19 18:16:39 +0900

    feat: Organization 전체 조회, 수정, 삭제 기능 추가

--- src/main/java/com/pangyopeoplebackend/organization/service/OrganizationService.java
+++ src/main/java/com/pangyopeoplebackend/organization/service/OrganizationService.java
@@ -4,11 +4,11 @@ import com.pangyopeoplebackend.domain.OrgCategory;
 import com.pangyopeoplebackend.organization.Organization;
 import com.pangyopeoplebackend.organization.repository.OrganizationRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -26,5 +26,33 @@ public class OrganizationService {
     @Transactional
     public Organization saveOrg(Organization organization) {
         return organizationRepository.save(organization);
+    }
+
+    @Transactional
+    public Optional<Organization> updateOrganization(String id, Organization organization) {
+        Optional<Organization> updateOrganization = organizationRepository.findById(id);
+
+        updateOrganization.ifPresent(selectOrganization->{
+            if(organization.getOrgName()!=null)
+                selectOrganization.setOrgName(organization.getOrgName());
+            if(organization.getOrgDescription()!=null)
+                selectOrganization.setOrgDescription(organization.getOrgDescription());
+            if(organization.getOrgUrl()!=null)
+                selectOrganization.setOrgUrl(organization.getOrgUrl());
+            if(organization.getOrgCategory()!=null)
+                selectOrganization.setOrgCategory(organization.getOrgCategory());
+
+            organizationRepository.save(selectOrganization);
+        });
+        return updateOrganization;
+    }
+
+    @Transactional
+    public Organization deleteOrganization(String id){
+        Optional<Organization> updateOrganization = organizationRepository.findById(id);
+        updateOrganization.ifPresent(selectOrganization->{
+            organizationRepository.delete(selectOrganization);
+        });
+        return updateOrganization.get();
     }
 }
