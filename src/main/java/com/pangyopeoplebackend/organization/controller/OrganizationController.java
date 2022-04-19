8bade8eb455bbabd9a22f9b36c3b0cfb7b87b7c0
Ayoon-b <wnr0303@gmail.com>
2022-04-19 18:16:51 +0900

    feat: Organization 전체 조회, 수정, 삭제 컨트롤러 추가

--- src/main/java/com/pangyopeoplebackend/organization/controller/OrganizationController.java
+++ src/main/java/com/pangyopeoplebackend/organization/controller/OrganizationController.java
@@ -1,14 +1,13 @@
 package com.pangyopeoplebackend.organization.controller;
 
 import com.pangyopeoplebackend.domain.OrgCategory;
-import com.pangyopeoplebackend.event.Event;
-import com.pangyopeoplebackend.event.service.EventService;
 import com.pangyopeoplebackend.organization.Organization;
 import com.pangyopeoplebackend.organization.service.OrganizationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,6 +18,20 @@ public class OrganizationController {
     @GetMapping("/v1/api/org/{category}")
     public List<Organization> getOrganizations(@PathVariable("category") OrgCategory orgCategory){
         return organizationService.getOrganizationsByOrgCategory(orgCategory);
+    }
+
+    @GetMapping(path = "/v1/api/orgs")
+    public List<Organization> getOrganizations(){
+        return organizationService.getOrganizations();
+    }
+
+    @PutMapping("/v1/api/org/{id}")
+    public Optional<Organization> updateOrganization(@PathVariable String id, @RequestBody Organization organization){
+        return organizationService.updateOrganization(id, organization);
+    }
+    @DeleteMapping("/v1/api/org/{id}")
+    public Organization deleteOrganization(@PathVariable String id){
+        return organizationService.deleteOrganization(id);
     }
 
     @PostMapping("/v1/api/org/write")
