af4629c4217b1ce47c91d93553420cbe3abf6de4
Ayoon-b <wnr0303@gmail.com>
2022-05-02 21:39:09 +0900

    feat: 카테고리 테이블 추가

---
+++ src/main/java/com/pangyopeoplebackend/category/Category.java
@@ -0,0 +1,24 @@
+package com.pangyopeoplebackend.category;
+
+import lombok.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.Id;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+public class Category {
+    @Id
+    private int categoryId;
+
+    @Column(nullable = false)
+    private String categoryName;
+}
