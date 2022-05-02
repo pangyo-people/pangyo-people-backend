991dd77c9b82e767bfc6d1e92b619aa0f3c2ccfc
Ayoon-b <wnr0303@gmail.com>
2022-05-02 21:40:46 +0900

    feat: org 수정

--- src/main/java/com/pangyopeoplebackend/organization/repository/OrganizationRepository.java
+++ src/main/java/com/pangyopeoplebackend/organization/repository/OrganizationRepository.java
@@ -3,12 +3,11 @@ package com.pangyopeoplebackend.organization.repository;
 import com.pangyopeoplebackend.domain.OrgCategory;
 import com.pangyopeoplebackend.organization.Organization;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-
+@Repository
 public interface OrganizationRepository extends JpaRepository<Organization, String>{
-
     List<Organization> findByOrgCategoryOrderByOrgName(OrgCategory orgCategory);
 }
